c3d33f4e7e9208c19cc00afb93693319e2ccb97e
David Mendoza <david.mendoza@dualbootpartners.com>
2024-01-08 11:43:07 -0500

    Changed color palette

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -3,7 +3,10 @@ import styles from "./Footer.module.sass"
 export const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      <p>Future World © {new Date().getFullYear()}</p>
+      <p>
+        Future World © • with 💜 by David Mendoza and Platzi •{" "}
+        {new Date().getFullYear()}
+      </p>
     </footer>
   )
 }
